8a432e274b7d2a080810bb34ac60cfbc99ad48af
akoschnitzki <91989133+akoschnitzki@users.noreply.github.com>
2021-10-17 17:47:42 -0500

    Module 3 Lab Problems
    In this file, I was able to show the codes on how to solve the problems. For all the examples, you can be able to input a number or name so that you can be able to get the answer that you are looking for.

---
+++ Module 3- Lab.py
@@ -0,0 +1,73 @@
+#Name- Alexander Koschnitzki
+#Date- 10-12-21
+#Module 3- Lab
+
+#Problem Number 1
+print("Hello World")
+# This program is able to show any word that you input. 
+
+#Problem Number 2
+hi=("Hey There")
+name=(" Alex")
+greeting = hi + name
+print(greeting)
+#This program is able to show a greeting to someone when they input their
+#name into the program.
+
+#Problem Number 3
+hi=("Hey There")
+name=(" Professor Sunham Num")
+names= (" and Alex")
+greeting = hi + name + names
+print(greeting)
+#This program is able to show a greeting for two people by
+#inputting two different names into the program.
+
+#Problem Number 4
+r= int(input("    "))  #get the radius from a user
+p= 3.14
+a=p*r**2
+print("The area of the given radius ", a)
+#This program is able to show the area of a circle. You can
+#input the radius of any circle and it will find the area.
+
+#Problem Number 5
+m= int(input("  "))
+g= int(input("  "))
+mpg = m/g
+print(mpg)
+#This program is able to show you the MPG. Once you input
+#the miles and the gallons of gas, it will be able to compute
+#the answer.
+
+#Problen Number 6
+F = int(input("  "))
+C= (F-32)* 5/9
+print(C)
+#This program is able to convert any Fahrenheit temperature to
+#the Celsius temperature. Input the Fahrenheit and it will be able
+#to compute the degrees in Celsius.
+
+#Problem Number 7
+#0=Sunday
+#1=Monday
+#2=Tuesday
+#3=Wednesday
+#4=Thurday
+#5=Friday
+#6=Saturday
+#Start day =int(input("  "))
+#length= int(input("  "))
+#x= start + length 
+#y= Day%7
+#print(y)
+start =int("3")
+length= int("10")
+x= start + length
+y= x%7
+print(y)
+#This program is able to compute what day of the week 
+#you will get if you input a day and how many days you
+#will return by. This program is perfect for telling what day
+#you will return from a trip if you input the day you leave
+#and how many days you will be gone for. 
